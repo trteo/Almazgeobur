1ea6ccad4d842fb57040b18796e1efae0b90a567
ph4nt0mm <100.ac@mail.ru>
2024-11-22 10:02:45 +0300

    config: add envs

---
+++ settings/config.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str = ''
+    POSTGRES_PASSWORD: str = ''
+    POSTGRES_DATABASE: str = ''
+    POSTGRES_HOST: str = ''
+    POSTGRES_PORT: str = ''
+
+    class Config:
+        env_file = Path(BASE_DIR, 'settings', 'env')
+
+
+settings = Settings()
+
+
+def pg_conn():
+    return {
+        'host': settings.POSTGRES_HOST,
+        'port': settings.POSTGRES_PORT,
+        'user': settings.POSTGRES_USER,
+        'password': settings.POSTGRES_PASSWORD,
+        'database': settings.POSTGRES_DATABASE,
+    }
+
+
+def get_db_url(db_name: str = settings.POSTGRES_DATABASE) -> str:
+    return (
+        f'postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@'
+        f'{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{db_name}'
+    )
+
+
+TORTOISE_ORM = {
+    'connections': {
+        'default': {
+            'engine': 'tortoise.backends.asyncpg',
+            'credentials': pg_conn(),
+        }
+    },
+    'apps': {
+        'models': {
+            'models': ['app.database', 'aerich.models'],
+            'default_connection': 'default'
+        }
+    }
+}
